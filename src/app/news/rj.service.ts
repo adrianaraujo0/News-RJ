f3a2f76f9eade61fc9e5c3ad2efcc3ffa946960b
adrianaraujo0 <adrianhenriquea4@gmail.com>
2024-05-15 19:17:54 -0300

    data

--- src/app/news/rj.service.ts
+++ src/app/news/rj.service.ts
@@ -10,15 +10,17 @@ export class RjService {
   private apiKey = "fbe2fb09498146e2af3c97a291ea71ee";
   private baseUrl = "https://newsapi.org/v2/everything";
   private query = "Rio de Janeiro";
-  private fromDate = "21-04-2024"; // Data mínima de publicação dos artigos
   private sortBy = "publishedAt"; // Classificar por data de publicação
 
   constructor(public http: HttpClient) { }
 
   public getNewsRj() {
-    const url = `${this.baseUrl}?q=${this.query}&from=${this.fromDate}&sortBy=${this.sortBy}&apiKey=${this.apiKey}`;
+    const url = `${this.baseUrl}?q=${this.query}&sortBy=${this.sortBy}&apiKey=${this.apiKey}`;
     return this.http.get(url);
   }
 }
 
 //https://newsapi.org/v2/everything?q=Rio%20de%20Janeiro&from=2024-04-12&sortBy=publishedAt&apiKey=fbe2fb09498146e2af3c97a291ea71ee
+
+
+//https://newsapi.org/v2/everything?q=Rio%20de%20Janeiro&sortBy=publishedAt&apiKey=fbe2fb09498146e2af3c97a291ea71ee
