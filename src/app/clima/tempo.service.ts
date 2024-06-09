56a31e84c50436d602b684092b9c501fdad12328
adrianaraujo0 <adrianhenriquea4@gmail.com>
2024-06-09 12:21:27 -0300

    metodos novos

--- src/app/clima/tempo.service.ts
+++ src/app/clima/tempo.service.ts
@@ -11,13 +11,13 @@ export class TempoService {
 
   constructor(private http: HttpClient) { }
 
-  obterClimaPorCoordenadas(lat: number, lon: number): Observable<any> {
-    return this.http.get(`${this.urlApi}?lat=${lat}&lon=${lon}&appid=${this.chaveApi}&units=metric&lang=pt_br`);
-  }
-
   obterClimaPorCidade(cidade: string): Observable<any> {
     return this.http.get(`${this.urlApi}?q=${cidade}&appid=${this.chaveApi}&units=metric&lang=pt_br`);
   }
 }
 
 //https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=47cda077cad71fc622b438aa037096e7&units=metric&lang=pt_br
+
+//obterClimaPorCoordenadas(lat: number, lon: number): Observable<any>{
+//return this.http.get(`${this.urlApi}?lat=${lat}&lon=${lon}&appid=$//{this.chaveApi}&units=metric&lang=pt_br`);
+//}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -11,12 +11,15 @@ export class Tab1Page {
   nomeCidade: string = ''; 
   dadosMeteorologicos: any;
   assinaturaMeteorologica: Subscription | undefined;
+  erro: string = '';
+  
 
   constructor(private tempoService: TempoService) {}
 
   pesquisarClima() {
     if (this.nomeCidade.trim() !== '') { 
       this.dadosMeteorologicos = null;
+      this.erro = '';
       
       this.assinaturaMeteorologica = this.tempoService.obterClimaPorCidade(this.nomeCidade).subscribe({
         next: (data) => {
@@ -24,8 +27,15 @@ export class Tab1Page {
         },
         error: (error) => {
           console.error('Erro ao obter dados meteorológicos', error);
-        }
+          if (error.status === 404) {
+            this.erro = 'Cidade não encontrada. Por favor, verifique o nome da cidade e tente novamente.';
+          } else {
+            this.erro = 'Erro ao obter dados meteorológicos. Por favor, tente novamente mais tarde.';
+          }
+        } 
       });
+    } else {
+      this.erro = 'Por favor, insira o nome da cidade.';
     }
   }
 
